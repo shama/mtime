eda6dafbd34e09f51e0c9afd132b8f30044075ea
Kyle Robinson Young <kyle@dontkry.com>
2013-10-01 16:05:13 -0700

    initial commit

---
+++ README.md
@@ -0,0 +1,13 @@
+# mtime
+
+Currently a work in progress.
+
+## Contributing
+In lieu of a formal styleguide, take care to maintain the existing coding style. Add unit tests for any new or changed functionality. Lint and test your code using [Grunt](http://gruntjs.com/).
+
+## Release History
+_(Nothing yet)_
+
+## License
+Copyright (c) 2013 Kyle Robinson Young  
+Licensed under the MIT license.
---
+++ test/mtime_test.js
@@ -0,0 +1,46 @@
+'use strict';
+
+var path = require('path');
+var fs = require('fs');
+var MTime = require('../lib/mtime.js');
+var grunt = require('grunt');
+var touch = require('touch').sync;
+var readdir = require('readdirrsync');
+
+function resetTimes(older, newer, cb) {
+  readdir(older).forEach(function(file) {
+    touch(file, {mtime: true});
+  });
+  setTimeout(function() {
+    readdir(newer).forEach(function(file) {
+      touch(file, {mtime: true});
+    });
+    cb();
+  }, 100);
+}
+
+exports.mtime = {
+  'setUp': function(done) {
+    this.cwd = process.cwd();
+    process.chdir(path.resolve(__dirname, 'fixtures'));
+    resetTimes('older', 'compare', done);
+  },
+  'tearDown': function(done) {
+    process.chdir(this.cwd);
+    done();
+  },
+  'compare': function(test) {
+    test.expect(1);
+
+    touch(path.resolve('older/one.css'), { mtime: new Date(1982, 8, 22) });
+
+    var mtime = new MTime();
+    var result = mtime.older(['one.css', 'two.css', 'sub/one.css'], 'compare', { cwd: 'older' });
+    var expected = ['one.css', 'three.css', 'sub/two.css'];
+    result.sort();
+    expected.sort();
+
+    test.deepEqual(result, expected);
+    test.done();
+  },
+};
